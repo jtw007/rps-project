8c78a8d4e013796f7067fa1bd73618ed5d0a9b1a
Josh <joshwu89@yahoo.com>
2022-09-22 20:49:02 -0700

    some progress

--- javascript.js
+++ javascript.js
@@ -1,49 +1,40 @@
-let getComputerChoice = Math.floor(Math.random());
+let playerSelection = ['Rock', 'Paper', 'Scissors'];
+let computerSelection = ['Rock', 'Paper', 'Scissors'];
 
-
-function getComputerChoice() {
-    console.log('Rock');
-    console.log('Paper');
-    console.log('Scissors');
-}
-    
-
-function playerSelection () {
-
+function playerSelection() {
+    console.log(playerSelection[0]);
 }
 
-function computerSelection () {
-
+function computerSelection() {
+    console.log(computerSelection[1])
 }
 
 function playRound(playerSelection, computerSelection) {
-if (playerSelection === Rock && computerSelection === Paper){
-    console.log("You lose! Paper beats rock!");
+    if (playerSelection === Rock && computerSelection === Paper){
+        console.log("You lose! Paper beats rock!");
 }
-else if (playerSelection === Rock && computerSelection === Rock) {
-    console.log("It's a draw!");
+    else if (playerSelection === Rock && computerSelection === Scissors) {
+        console.log('You win! Rock beats scissors!');
+}      
+    else if (playerSelection === Rock && computerSelection === Rock) {
+        console.log("It's a draw!");
+} 
+    else if (playerSelection === Paper && computerSelection === Scissors) {
+        console.log("You lose! Scissors beats paper!");
+}  
+    else if (playerSelection === Paper && computerSelection === Rock){
+        console.log('You win! Paper beats rock!');
 }
-else if (playerSelection === Rock && computerSelection === Scissors) {
-    console.log('You win! Rock beats scissors!');
+    else if (playerSelection === Paper && computerSelection === Paper) {
+        console.log("It's a draw!");
 }
-else if (playerSelection === Paper && computerSelection === Rock){
-    console.log('You win! Paper beats rock!');
+    else if (playerSelection === Scissors && computerSelection === Rock) {
+        console.log("You lose! Rock beats scissors!");
 }
-else if (playerSelection === Paper && computerSelection === Paper) {
-    console.log("It's a draw!");
+    else if (playerSelection === Scissors && computerSelection === Paper) {
+        console.log("You win! Scissors beats paper!");
 }
-else if (playerSelection === Paper && computerSelection === Scissors) {
-    console.log("You lose! Scissors beats paper!");
-}
-else if (playerSelection === Scissors && computerSelection === Rock) {
-    console.log("You lose! Rock beats scissors!");
-}
-else if (playerSelection === Scissors && computerSelection === Paper) {
-    console.log("You win! Scissors beats paper!");
-}
-else if (playerSelection === Scissors && computerSelection ===Scissors) {
-    console.log("It's a draw!");
+    else if (playerSelection === Scissors && computerSelection ===Scissors) {
+        console.log("It's a draw!");
 }
 }
-
-
